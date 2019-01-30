531d33e03119545ec8b2bb1601d21cf67ebe99df
Gil Katchko <gil.katchko@gmail.com>
2019-01-30 16:14:52 +0200

    Set test database and config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise // Set mongoose to use default promise library
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }) // Connect to db with mongoose
+mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true }) // Connect to db with mongoose
 mongoose.set('useFindAndModify', false)
 
 module.exports = { mongoose }
