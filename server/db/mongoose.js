026037068339dcc442d0f364964525eb6c2a464d
Gil Katchko <gil.katchko@gmail.com>
2019-01-30 14:51:48 +0200

    Add PATCH /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise // Set mongoose to use default promise library
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }) // Connect to db with mongoose
+mongoose.set('useFindAndModify', false)
 
 module.exports = { mongoose }
