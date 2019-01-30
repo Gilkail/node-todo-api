975b7eaf847df2094350b620e5bc998a4f54c1bc
Gil Katchko <gil.katchko@gmail.com>
2019-01-30 16:27:56 +0200

    Added comments

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,10 @@
-const env = process.env.NODE_ENV || 'development'
+const env = process.env.NODE_ENV || 'development' // Check if NODE_ENV already set if not set to development
 console.log('env:', env)
 
-if(env === 'development') {
-    process.env.PORT = 3000
+if(env === 'development') { //If development set port to 3000 and db to development db
+    process.env.PORT = 3000 
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
-} else if (env === 'test'){
+} else if (env === 'test'){ // If test set port to 3000 and db to test db
     process.env.PORT = 3000
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
 }
