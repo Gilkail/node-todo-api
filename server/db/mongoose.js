6c1ecc78f5386c7ab13ceed52eb2610672f2cb52
Gil Katchko <gil.katchko@gmail.com>
2019-01-31 16:42:26 +0200

    Adding user tokens

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,5 +3,6 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise // Set mongoose to use default promise library
 mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true }) // Connect to db with mongoose
 mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
 
 module.exports = { mongoose }
