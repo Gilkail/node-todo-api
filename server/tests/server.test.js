7c06b7af435c0d150c47824f40d929cb080a1365
Gil Katchko <gil.katchko@gmail.com>
2019-02-04 16:02:40 +0200

    Adding DELETE /users/me/tokens test case

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -294,3 +294,23 @@ describe('POST /users/login', ()=>{
             })
     })
 })
+
+describe('DELETE /users/me/tokens', ()=>{
+    it('Should remove x-auth token on logout', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res)=>{
+                if(err){
+                    return done(err)
+                }
+                
+                User.findById(users[0]._id).then((user)=>{
+                    expect(user.tokens.length).toBe(0)
+                    done()
+                }).catch((err)=>done(err))
+
+            })
+    })
+})
